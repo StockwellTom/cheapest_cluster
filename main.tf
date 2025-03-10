7ebc07384eb145f99cbbecf685086488b34c8d90
Tom Stockwell <Tom.Stockwell@bailliegifford.com>
2025-03-10 13:17:31 +0000

    added resource_provider_registrations = "none"

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 provider "azurerm" {
   features {}
   subscription_id= "b133cf1b-9061-473a-a041-99c71f10c773"
+  resource_provider_registrations = "none"
 }
 
 resource "azurerm_resource_group" "main_cluster" {
