e3ad2c3c56cd0536c932aaad467657fa2d0e083b
Tom Stockwell <Tom.Stockwell@bailliegifford.com>
2025-03-11 10:33:44 +0000

    deleted sample deployment

--- providers.tf
+++ providers.tf
@@ -4,9 +4,9 @@ provider "azurerm" {
   resource_provider_registrations = "none"
 }
 
-provider "kubernetes" {
-  host                   = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.host
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate)
-}
+# provider "kubernetes" {
+#   host                   = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.host
+#   client_certificate     = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate)
+#   client_key             = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_key)
+#   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate)
+# }
