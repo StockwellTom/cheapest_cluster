5562343e9b0ce077a4f143ec11d84cb21e2c4cde
Tom Stockwell <Tom.Stockwell@bailliegifford.com>
2025-03-12 14:19:45 +0000

    using MI instead

--- turn_off_cluster.tf
+++ turn_off_cluster.tf
@@ -1,18 +1,11 @@
-data "azuread_client_config" "current" {}
-
-resource "azuread_application" "cluster_app" {
-  display_name = "turn_off_cluster_app"
-  owners       = [data.azuread_client_config.current.object_id]
-}
-
-resource "azuread_service_principal" "cluster_app_sp" {
-  client_id                    = azuread_application.cluster_app.client_id
-  app_role_assignment_required = false
-  owners                       = [data.azuread_client_config.current.object_id]
+resource "azurerm_user_assigned_identity" "cluster_mi" {
+  location            = azurerm_resource_group.main_cluster.location
+  name                = "cluster_contributor"
+  resource_group_name = azurerm_resource_group.main_cluster.name
 }
 
 resource "azurerm_role_assignment" "aks_sp_role_assignment" {
   scope                = azurerm_kubernetes_cluster.aks_cluster.id
   role_definition_name = "Contributor"
-  principal_id         = azuread_service_principal.cluster_app_sp.client_id
+  principal_id         = azurerm_user_assigned_identity.cluster_mi.principal_id
 }
