da6238e1a3d35a779bb77e1e34b332af94a5aed9
Tom Stockwell <Tom.Stockwell@bailliegifford.com>
2025-03-13 11:11:26 +0000

    added federated cred resource

--- python/turn_off_cluster/turn_off_cluster.py
+++ python/turn_off_cluster/turn_off_cluster.py
@@ -3,9 +3,9 @@ from azure.mgmt.containerservice import ContainerServiceClient
 
 # Initialize the credential and client
 credential = DefaultAzureCredential()
-subscription_id = "<YourSubscriptionId>"
-resource_group = "<YourResourceGroupName>"
-cluster_name = "<YourClusterName>"
+subscription_id = "b133cf1b-9061-473a-a041-99c71f10c773"
+resource_group = "main_cluster"
+cluster_name = "my-aks-cluster"
 
 # Create a ContainerServiceClient
 client = ContainerServiceClient(credential, subscription_id)
--- turn_off_cluster.tf
+++ turn_off_cluster.tf
@@ -9,3 +9,12 @@ resource "azurerm_role_assignment" "aks_sp_role_assignment" {
   role_definition_name = "Contributor"
   principal_id         = azurerm_user_assigned_identity.cluster_mi.principal_id
 }
+
+resource "azurerm_federated_identity_credential" "turn-off-cluster-federated-credential" {
+  name                = "fc-turn-off-cluster"
+  resource_group_name = "main_cluster"
+  audience            = ["api://AzureADTokenExchange"]
+  issuer              = "https://westeurope.oic.prod-aks.azure.com/9c3053bb-1522-475e-82b2-b426b68a8004/61cd0ea9-54c9-4949-875f-530f3013b097/"
+  parent_id           = azurerm_user_assigned_identity.cluster_mi.id
+  subject             = "system:serviceaccount:turn-off-cluster:turn-off-cluster-mi"
+}
