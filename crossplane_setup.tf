de8a5963455bcc002f144c0e43408d247ecdf2f5
Tom Stockwell <Tom.Stockwell@bailliegifford.com>
2025-03-17 11:55:50 +0000

    fixed secret syntax

--- crossplane_setup.tf
+++ crossplane_setup.tf
@@ -44,6 +44,6 @@ resource "azurerm_key_vault" "crossplane-kv" {
 
 resource "azurerm_key_vault_secret" "crossplane-kv-secret" {
   name         = "crossplane-password"
-  value        = azuread_service_principal_password.crossplane-sp-password-assign.result
+  value        = azuread_service_principal_password.crossplane-sp-password-assign.vault
   key_vault_id = azurerm_key_vault.crossplane-kv.id
 }
