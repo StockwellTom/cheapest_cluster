910392e24572937686e084191cfb26b26739491e
Tom Stockwell <Tom.Stockwell@bailliegifford.com>
2025-03-17 17:17:07 +0000

    kubelit id

--- crossplane_setup.tf
+++ crossplane_setup.tf
@@ -1,4 +1,4 @@
-data "azurerm_user_assigned_identity" "cluster_mi" {
+data "azurerm_user_assigned_identity" "kubelet_id" {
   name                = "my-aks-cluster-agentpool" # maybe try not hard code
   resource_group_name = azurerm_kubernetes_cluster.aks_cluster.node_resource_group
 }
@@ -6,5 +6,5 @@ data "azurerm_user_assigned_identity" "cluster_mi" {
 resource "azurerm_role_assignment" "crossplane_mi_owner" {
   scope                = "/subscriptions/b133cf1b-9061-473a-a041-99c71f10c773"
   role_definition_name = "Owner"
-  principal_id         = data.azurerm_user_assigned_identity.cluster_mi.principal_id
+  principal_id         = data.azurerm_user_assigned_identity.kubelet_id.principal_id
 }
