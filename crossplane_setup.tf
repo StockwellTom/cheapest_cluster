124dddb8e3ac21ce6ff9768df72d4b0bff73fe37
Tom Stockwell <Tom.Stockwell@bailliegifford.com>
2025-03-17 16:23:11 +0000

    change to mi

--- crossplane_setup.tf
+++ crossplane_setup.tf
@@ -1,50 +1,11 @@
-resource "azuread_application" "crossplane-app" {
-  display_name = "crossplane"
-}
-
-resource "azuread_service_principal" "crossplane-sp" {
-  client_id = azuread_application.crossplane-app.client_id
-}
-
-resource "azuread_service_principal_password" "crossplane-sp-password-assign" {
-  service_principal_id = azuread_service_principal.crossplane-sp.id
-}
-
-resource "azurerm_role_assignment" "crossplane-ra" {
-  principal_id   = azuread_service_principal.crossplane-sp.object_id
-  role_definition_name = "Owner"
-  scope          = "/subscriptions/b133cf1b-9061-473a-a041-99c71f10c773"
-}
-
-resource "random_string" "suffix" {
-  length  = 6
-  special = false
-  upper   = false
-}
-
-resource "azurerm_key_vault" "crossplane-kv" {
-  name                = "kv-crossplane-${random_string.suffix.result}"
+resource "azurerm_user_assigned_identity" "crossplane_mi" {
   location            = azurerm_resource_group.main_cluster.location
+  name                = "cluster_contributor"
   resource_group_name = azurerm_resource_group.main_cluster.name
-
-  tenant_id = "9c3053bb-1522-475e-82b2-b426b68a8004"
-
-  sku_name = "standard"
-
-  access_policy {
-    tenant_id = "9c3053bb-1522-475e-82b2-b426b68a8004"
-    object_id = "1a45d9e9-6f0d-44f0-8154-233711b1ad2f" # Replace with your Azure AD object ID
-
-    secret_permissions = [
-      "Get",
-      "List",
-      "Set"
-    ]
-  }
 }
 
-resource "azurerm_key_vault_secret" "crossplane-kv-secret" {
-  name         = "crossplane-password"
-  value        = azuread_service_principal_password.crossplane-sp-password-assign.value
-  key_vault_id = azurerm_key_vault.crossplane-kv.id
+resource "azurerm_role_assignment" "crossplane_mi_owner" {
+  scope                = "/subscriptions/b133cf1b-9061-473a-a041-99c71f10c773"
+  role_definition_name = "Owner"
+  principal_id         = azurerm_user_assigned_identity.crossplane_mi.principal_id
 }
